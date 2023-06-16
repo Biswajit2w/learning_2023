d1c71741a65b98ceb207d07da36018f1a8817a9d
Biswajit Sahoo <biswajitsahoo2w@gmail.com>
2023-06-16 09:03:06 +0530

    Adding solution level1

---
+++ module1/Day1/exp4.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+int main()
+{
+    float operand1, operand2, result;
+    char operator;
+
+    printf("Enter Number 1: ");
+    scanf("%f", &operand1);
+
+    printf("Enter the operator : ");
+    scanf(" %c", &operator); 
+
+    printf("Enter Number 2: ");
+    scanf("%f", &operand2);
+
+    switch (operator)
+    {
+        case '+':
+            result = operand1 + operand2;
+            break;
+        case '-':
+            result = operand1 - operand2;
+            break;
+        case '*':
+            result = operand1 * operand2;
+            break;
+        case '/':
+                result = operand1 / operand2;
+            break;
+        default:
+            printf("Error: Invalid operator\n");
+            return 1; 
+    }
+
+    printf("Result: %.2f\n", result);
+
+    return 0;
+}
+
